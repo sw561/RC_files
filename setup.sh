774c4e22353c869d699faadacc7fd36bcf24a0c8
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2018-04-08 12:03:18 +0100

    Organise vim files into directories mirroring the .vim structure

--- setup.sh
+++ setup.sh
@@ -5,23 +5,11 @@ ln -s RC_files/.tmux.conf .
 ln -s RC_files/.git-prompt.sh .
 ln -s RC_files/.Xmodmap .
 mkdir -p .vim
-mkdir -p .vim/syntax
-ln -s ~/RC_files/swig.vim .vim/syntax
-mkdir -p .vim/plugin
-ln -s ~/RC_files/tabline.vim .vim/plugin
-ln -s ~/RC_files/statusline.vim .vim/plugin
-ln -s ~/RC_files/trim.vim .vim/plugin
-ln -s ~/RC_files/long_lines.vim .vim/plugin
-mkdir -p .vim/colors
-ln -s ~/RC_files/sand.vim .vim/colors
-mkdir -p .vim/ftplugin
-ln -s ~/RC_files/python.vim .vim/ftplugin
-ln -s ~/RC_files/go.vim .vim/ftplugin
-ln -s ~/RC_files/cpp.vim .vim/ftplugin
-ln -s ~/RC_files/git.vim .vim/ftplugin
-mkdir -p .vim/after
-mkdir -p .vim/after/syntax
-ln -s ~/RC_files/diff.vim .vim/after/syntax
+ln -s ~/RC_files/vim/syntax/ .vim
+ln -s ~/RC_files/vim/plugin/ .vim
+ln -s ~/RC_files/vim/colors/ .vim
+ln -s ~/RC_files/vim/ftplugin/ .vim
+ln -s ~/RC_files/vim/after/ .vim
 ln -s ~/RC_files/tmux_move.py bin
 ln -s ~/RC_files/move_left bin
 ln -s ~/RC_files/move_right bin
