b5a23c94951c2fca6419b21070d07aee959acad5
S.D. Wilkinson <sw561@cam.ac.uk>
2018-05-02 17:40:18 +0100

    Refactor VP module
    Use custom context manager for file handling, also use library functions
    for handling system paths. Use python regex module as opposed to
    using grep via bash.

--- vp.py
+++ vp.py
@@ -10,22 +10,22 @@ All it does is generate bash scripts, which you don't need to execute until you
 are satisfied that nothing unexpected is about to happen.
 """
 
-import argparse
-from subprocess import check_output
-import os
-# To ensure python2 compatibility
+from __future__ import print_function
 from six.moves import input
+from contextlib import contextmanager
+import os
+import re
 
 COMMAND_FILE = "AUTOvp.sh"
-BUNDLE_PATH = ".vim/bundle"
+BUNDLE_PATH = ".vim/bundle/"
+HOME = os.path.expanduser("~")
+VIMRC_PATH = os.path.join(HOME, ".vimrc")
+
 GIT_OPTIONS = "--depth 1"
 DIVIDER = "-"*40
 
-def home():
-    """ Return path to home directory """
-    return os.path.expanduser("~")
-
-def myopen(filename, method):
+@contextmanager
+def myopen(filename, *args, **kwargs):
     """
     Open a file for writing, and ensure we are not going to overwrite something
     important
@@ -37,31 +37,35 @@ def myopen(filename, method):
         except KeyboardInterrupt:
             print('')
             exit()
-    return open(filename, method)
+    f = open(filename, *args, **kwargs)
+    try:
+        yield f
+        print("Commands to be run have been written in {}".format(COMMAND_FILE))
+        print("To run, type $ bash {}".format(COMMAND_FILE))
+    finally:
+        f.close()
 
-def split_pairs(text, expected_prefix):
-    # Yield lines with the expected_prefix and their preceding comments
-    u = text.split('\n')
-    for i in range(1, len(u)):
-        if u[i].startswith(expected_prefix) and u[i-1] and u[i-1][0]=='"':
-            yield u[i-1], u[i]
+def get_pairs(expected_prefix):
+    with open(VIMRC_PATH) as f:
+        prev = None
+        for line in f:
+            line = line.strip()
+            if re.match(expected_prefix, line) and re.match('^"', prev):
+                yield prev, line
+            prev = line
 
 def get_plugins():
     """
     Get details of all the desired plugins from the .vimrc file
     """
+    expected_prefix = "^set runtimepath\+=~/{}".format(BUNDLE_PATH)
 
-    expected_prefix = "set runtimepath+=~/{}/".format(BUNDLE_PATH)
-    command = "grep -B1 '^{}' {}/.vimrc".format(expected_prefix, home())
-    # print("command:", command)
-    grep_output = check_output(command, shell=True).decode('utf8').strip()
-
-    for comment, rtpath in split_pairs(grep_output, expected_prefix):
+    for comment, rtpath in get_pairs(expected_prefix):
 
         url = comment[1:].lstrip()
         name_from_url = "/".join(url.split()[0].split('/')[3:])
-        name_from_path = rtpath[len(expected_prefix):]
-        if not name_from_url.endswith(name_from_path+'.git'):
+        name_from_path = rtpath[len(expected_prefix)-2:]
+        if not name_from_url.split("/")[1] == name_from_path+'.git':
             print("Failed to understand the lines:\n{}\n{}".format(comment, rtpath))
             print("name_from_url:", name_from_url)
             print("name_from_path:", name_from_path)
@@ -70,13 +74,9 @@ def get_plugins():
         # Remove the trailing .git
         name_from_url = name_from_url[:-4]
 
-        path = "/".join([home(), BUNDLE_PATH, name_from_path])
+        path = os.path.join(HOME, BUNDLE_PATH, name_from_path)
 
         yield (name_from_url, url, path)
-
-def finish():
-    print("Commands to be run have been written in {}".format(COMMAND_FILE))
-    print("To run, type $ bash {}".format(COMMAND_FILE))
 
 def clean_up():
     unneeded_plugins = []
@@ -84,7 +84,7 @@ def clean_up():
     # First find paths that we want to keep
     paths = set(x[2].split('/')[-1] for x in get_plugins())
 
-    for path in os.listdir("{}/{}".format(home(), BUNDLE_PATH)):
+    for path in os.listdir(os.path.join(HOME, BUNDLE_PATH)):
         if path not in paths:
             print("{:40s} no longer needed".format(path))
             unneeded_plugins.append(path)
@@ -94,13 +94,10 @@ def clean_up():
         return
 
     print(DIVIDER)
-    f = myopen(COMMAND_FILE, 'w')
-    f.write("set -e\n")
-    for path in unneeded_plugins:
-        f.write("rm -rf {}/{}/{}\n".format(home(), BUNDLE_PATH, path))
-    f.close()
-
-    finish()
+    with myopen(COMMAND_FILE, 'w') as f:
+        f.write("set -e\n")
+        for path in unneeded_plugins:
+            f.write("rm -rf {}\n".format(os.path.join(HOME, BUNDLE_PATH, path)))
 
 def main():
     urls = []
@@ -126,22 +123,19 @@ def main():
     print(DIVIDER)
 
     # Ensure the folder actually exists
-    bundle_dir = "{}/{}".format(home(), BUNDLE_PATH)
+    bundle_dir = os.path.join(HOME, BUNDLE_PATH)
     if not os.path.exists(bundle_dir):
         os.makedirs(bundle_dir)
 
-    f = myopen(COMMAND_FILE, 'w')
-    f.write("set -e\n")
-    f.write("cd {}/{}\n".format(home(), BUNDLE_PATH))
-    for url in urls:
-        f.write("git clone {} {}\n".format(GIT_OPTIONS, url))
+    with myopen(COMMAND_FILE, 'w') as f:
+        f.write("set -e\n")
+        f.write("cd {}\n".format(os.path.join(HOME, BUNDLE_PATH)))
+        for url in urls:
+            f.write("git clone {} {}\n".format(GIT_OPTIONS, url))
 
-    # Helptags command
-    for path in paths:
-        f.write('vim -u NONE -c "helptags {}/doc" -c q\n'.format(path))
-    f.close()
-
-    finish()
+        # Helptags command
+        for path in paths:
+            f.write('vim -u NONE -c "helptags {}" -c q\n'.format(os.path.join(path, "doc")))
 
 def update():
     urls = []
@@ -155,17 +149,14 @@ def update():
             plugins.append(name)
             paths.append(path)
 
-    f = myopen(COMMAND_FILE, 'w')
-    f.write("set -e\n")
-    for p in paths:
-        f.write("cd {}\n".format(p))
-        f.write("git pull\n")
-
-    f.close()
-
-    finish()
+    with myopen(COMMAND_FILE, 'w') as f:
+        f.write("set -e\n")
+        for p in paths:
+            f.write("cd {}\n".format(p))
+            f.write("git pull\n")
 
 if __name__=="__main__":
+    import argparse
     parser = argparse.ArgumentParser(
         description="vp is a non-scary script to install your vim plugins"
         )
